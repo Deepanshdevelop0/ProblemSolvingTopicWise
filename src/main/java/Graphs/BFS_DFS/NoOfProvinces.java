68c21108d3a5b64529d9d9d79062fb6d05a20bdf
Deepanshu Tilwani <shonatilwani00@gmail.com>
2025-07-22 00:33:27 +0530

    added findIfPathExistsInGraph

--- src/main/java/Graphs/BFS_DFS/NoOfProvinces.java
+++ src/main/java/Graphs/BFS_DFS/NoOfProvinces.java
@@ -1,15 +1,80 @@
 package Graphs.BFS_DFS;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class NoOfProvinces {
 
     public static void main(String[] args) {
+        // adjacency matrix
+        int[][] isConnected = new int[3][3];
 
+        // Populate the 2D array with the given values
+        isConnected[0][0] = 1;
+        isConnected[0][1] = 0;
+        isConnected[0][2] = 0;
+
+        isConnected[1][0] = 0;
+        isConnected[1][1] = 1;
+        isConnected[1][2] = 0;
+
+        isConnected[2][0] = 0;
+        isConnected[2][1] = 0;
+        isConnected[2][2] = 1;
+
+
+        System.out.println(findCircleNum(isConnected));
     }
 
-    public int findCircleNum(int[][] isConnected) {
-        return 0;
+    public static int findCircleNum(int[][] isConnected) {
+
+        int V = isConnected.length;
+
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+
+        for (int i = 0; i <= V; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        /* Convert the adjacencyMatrix to adjacencyList */
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (isConnected[i][j] == 1 && i != j) {
+                    adjList.get(i).add(j);
+                    adjList.get(j).add(i);
+                }
+            }
+        }
+
+
+        int count = 0;
+        boolean[] visited = new boolean[V + 1];
+
+        /* for each index in the adjList to iterate */
+        for (int i = 0; i < V; i++) {
+            /* call the dfs only when previous province is already traversed (curr node is unvisited),
+                or in starting pos */
+            if (!visited[i]) {
+                dfs(i, visited, adjList);
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void dfs(int curr, boolean[] visited, ArrayList<ArrayList<Integer>> adjList) {
+
+        List<Integer> currAdjList = adjList.get(curr);
+
+        visited[curr] = true;
+
+        for (int i : currAdjList) {
+            if (!visited[i]) {
+                dfs(i, visited, adjList);
+            }
+        }
+
     }
 
 
---
+++ src/main/java/Graphs/BFS_DFS/findIfPathExistsInGraph.java
@@ -0,0 +1,62 @@
+package Graphs.BFS_DFS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class findIfPathExistsInGraph {
+
+    public static void main(String[] args) {
+
+        int[][] edges = new int[3][3];
+
+        // Populate the 2D array with the given values
+        edges[0][0] = 0;
+        edges[0][1] = 1;
+
+        edges[1][0] = 1;
+        edges[1][1] = 2;
+
+        edges[2][0] = 2;
+        edges[2][1] = 0;
+
+        System.out.println(validPath(3, edges, 0, 2));
+
+    }
+
+    public static boolean validPath(int n, int[][] edges, int source, int destination) {
+
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for (int[] vert : edges) {
+            int u = vert[0], v = vert[1];
+            adjList.get(u).add(v);
+            adjList.get(v).add(u);
+        }
+
+        boolean[] visited = new boolean[n];
+
+        dfs(source, visited, adjList);
+
+
+        return visited[destination];
+    }
+
+    public static void dfs(int source, boolean[] visited, ArrayList<ArrayList<Integer>> adjList) {
+
+        List<Integer> currAdjList = adjList.get(source);
+
+        visited[source] = true;
+
+        for (int i : currAdjList) {
+            if (!visited[i]) {
+                dfs(i, visited, adjList);
+            }
+        }
+
+    }
+
+}
