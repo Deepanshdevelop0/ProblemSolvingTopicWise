d492565133f2ecc39ae58ca2f42f5c50375d8c3a
Deepanshu Tilwani <shonatilwani00@gmail.com>
2025-08-11 00:29:56 +0530

    added WordLadder2

--- src/main/java/Graphs/BFS_DFS/BFS_BASED/WordLadder2.java
+++ src/main/java/Graphs/BFS_DFS/BFS_BASED/WordLadder2.java
@@ -1,5 +1,6 @@
 package Graphs.BFS_DFS.BFS_BASED;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class WordLadder2 {
@@ -7,91 +8,76 @@ public class WordLadder2 {
     public static void main(String[] args) {
 
         String beginWord = "hit", endWord = "cog";
-        List<String> wordList2 = List.of("hot","dot","dog","lot","log","cog");
+        List<String> wordList = List.of("hot","dot","dog","lot","log","cog");
         List<String> wordList1 = List.of("hot","dot","dog","lot","log");
-        List<String> wordList = List.of("des", "der", "dfr", "dgt", "dfs");
 
-        String startWord = "der", targetWord = "dfs";
 
-        System.out.println(findLadders(startWord, targetWord, wordList));
+        System.out.println(findLadders(beginWord, endWord, wordList));
 
     }
 
     public static List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
 
         Set<String> wordSet = new HashSet<>(wordList);
-        Set<String> visitedSet = new HashSet<>();
 
-        Pair pair = new Pair(beginWord, 1);
+        Queue<ArrayList<String>> queue = new LinkedList<>();
+        ArrayList<String> list = new ArrayList<>();
+        list.add(beginWord);
+        queue.add(list);
 
-        wordSet.remove(beginWord);
+        ArrayList<String> usedOnLevel = new ArrayList<>();
+        usedOnLevel.add(beginWord);
+
+        int level = 0;
 
         List<List<String>> res = new ArrayList<>();
-        List<String> currList = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+
+            ArrayList<String> currList = queue.peek();
+            queue.remove();
+
+            if (currList.size() > level) {
+                level++;
+                for (String i : usedOnLevel) {
+                    wordSet.remove(i);
+                }
+            }
+
+            String word = currList.get(currList.size() - 1);
+
+            if (word.equals(endWord)) {
+                if (res.isEmpty()) res.add(list);
+                else if (res.get(0).size() == list.size()) res.add(list);
+            }
 
 
-        dfs(pair, endWord, visitedSet, wordSet, currList, res);
+            for (int i = 0; i < word.length(); i++) {
+
+                char[] chArr = word.toCharArray();
+
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    chArr[i] = ch;
+                    String temp = new String(chArr);
+
+                    if (wordSet.contains(temp)) {
+
+                        list.add(temp);
+
+                        queue.add(new ArrayList<>(list));
+
+                        usedOnLevel.add(temp);
+
+                        list.remove(list.size() - 1);
+
+                    }
+                }
+            }
+
+        }
+
 
         return res;
-    }
-
-
-    public static void dfs(Pair currPair, String endWord, Set<String> visitedSet, Set<String> wordSet, List<String> currList,
-                           List<List<String>> res) {
-
-        String word = currPair.word;
-        int steps = currPair.steps;
-
-        currList.add(word);
-
-        if (word.equals(endWord)) {
-            wordSet.add(endWord);
-            visitedSet.remove(endWord);
-            res.add(new ArrayList<>(currList));
-            return;
-        }
-
-        List<Pair> possibilities = new ArrayList<>();
-
-        for (int i = 0; i < word.length(); i++) {
-            char[] charr = word.toCharArray();
-
-            for (char ch = 'a'; ch <= 'z'; ch++) {
-                charr[i] = ch;
-                String temp = new String(charr);
-
-                if (wordSet.contains(temp)) {
-                    wordSet.remove(temp);
-                    possibilities.add(new Pair(temp, steps+1));
-                    if (temp.equals(endWord)) break;
-                }
-
-            }
-
-        }
-
-        for (Pair pair : possibilities) {
-            if (!visitedSet.contains(pair.word)) {
-                visitedSet.add(pair.word);
-                dfs(pair, endWord, visitedSet, wordSet, currList, res);
-                currList.remove(currList.size() - 1);
-            }
-        }
-
-
-    }
-
-    static class Pair {
-
-        String word;
-
-        int steps;
-
-        Pair(String word, int steps) {
-            this.word = word;
-            this.steps = steps;
-        }
-
     }
 
 
