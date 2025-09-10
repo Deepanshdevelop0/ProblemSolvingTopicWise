9d7569d6f7800a51ca375b15370562eafc9ac777
Deepanshu Tilwani <shonatilwani00@gmail.com>
2025-09-10 23:46:32 +0530

    added SerializeandDeserializeBinaryTree

--- src/main/java/Trees/BinaryTrees/Problems/SerializeandDeserializeBinaryTree.java
+++ src/main/java/Trees/BinaryTrees/Problems/SerializeandDeserializeBinaryTree.java
@@ -8,6 +8,22 @@ import java.util.*;
 
 public class SerializeandDeserializeBinaryTree {
 
+
+/*
+
+Preorder DFS
+
+TC : O(n)
+
+i. Each node is processed once → O(n).
+ii. Each StringBuilder.append is amortized O(1).
+
+SC : O(n + h) ~ O(n)
+
+i. StringBuilder stores n values + n "null" markers → O(n)
+ii. Recursion stack, for Balanced trees : O(h) ~ O(log n), for skewed trees in worst case O(n)
+
+*/
     public static void main(String[] args) {
         TreeNode root = TreeUtils.buildTree(new Integer[]{1,2,3,null,null,4,5});
 
@@ -37,31 +53,29 @@ public class SerializeandDeserializeBinaryTree {
         sb.append(root.val + ",");
         serializeDFS(root.left, sb);
         serializeDFS(root.right, sb);
-
     }
-
 
 
     // Decodes your encoded data to tree.
     public static TreeNode deserialize(String data) {
-
         String[] nodes = data.split(",");
-        Queue<String> queue = new LinkedList<>(Arrays.asList(nodes));
+        Queue<String> queue = new ArrayDeque<>(Arrays.asList(nodes));
         return deserializeDFS(queue);
     }
 
-
     public static TreeNode deserializeDFS(Queue<String> queue) {
-        String val = queue.poll();
-        if (val.equals("null")) {
+
+        String node = queue.poll();
+
+        if (node.equals("null")) {
             return null;
         }
 
-        TreeNode root = new TreeNode(Integer.parseInt(val));
+        TreeNode root = new TreeNode(Integer.parseInt(node));
+
         root.left = deserializeDFS(queue);
         root.right = deserializeDFS(queue);
         return root;
     }
-
 
 }
