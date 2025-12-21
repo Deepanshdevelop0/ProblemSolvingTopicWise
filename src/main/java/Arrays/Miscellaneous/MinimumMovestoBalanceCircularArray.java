e2f75e477ff7f4454418e936db0dd3ea4b9f56fc
Deepanshu Tilwani <shonatilwani00@gmail.com>
2025-12-21 23:33:32 +0530

    added MinimumMovestoBalanceCircularArray

--- src/main/java/Arrays/Miscellaneous/MinimumMovestoBalanceCircularArray.java
+++ src/main/java/Arrays/Miscellaneous/MinimumMovestoBalanceCircularArray.java
@@ -10,9 +10,12 @@ public class MinimumMovestoBalanceCircularArray {
     public static void main(String[] args) {
 
         int[] nums = new int[]{5, 1, -4};
+        int[] nums1 = new int[]{1, 2, -5, 2};
+        int[] nums2 = new int[]{2,43,41,4,30,0,-46};
+        int[] nums3 = new int[]{-10,4,5};
 
         MinimumMovestoBalanceCircularArray classObj = new MinimumMovestoBalanceCircularArray();
-        System.out.println(classObj.minMoves(nums));
+        System.out.println(classObj.minMoves(nums3));
     }
 
 
@@ -26,26 +29,38 @@ public class MinimumMovestoBalanceCircularArray {
             }
         }
 
-        int moves = 0;
+        long moves = 0;
 
-        for (int i = 1; i < n; i++) {
 
-            if ((negIndx - i < 0 && negIndx + i >= n) || neg == 0) {
+        int j = 0, k = 0, i = 0;
+
+        for (i = 1; i < n; i++) {
+
+            j = (negIndx - i >= 0) ? negIndx - i : n + (negIndx - i);
+            k = (negIndx + i < n) ? negIndx + i : (negIndx + i) - n;
+
+            if (j == k || neg == 0) {
                 break;
             }
 
-            if (negIndx - i >= 0) {
-                int min = Math.min(Math.abs(neg), balance[negIndx - i]);
-                moves += (min * i);
-                neg += min;
-            }
+            // for indx of j
+            int min = Math.min(Math.abs(neg), balance[j]);
+            moves += ((long) min * i);
+            neg += min;
+            balance[j] = 0;
 
-            if (negIndx + i < n) {
-                int min = Math.min(Math.abs(neg), balance[negIndx + i]);
-                moves += (min * i);
-                neg += min;
-            }
+            // for indx of k
+            min = Math.min(Math.abs(neg), balance[k]);
+            moves += ((long) min * i);
+            neg += min;
+            balance[k] = 0;
 
+        }
+
+        if (j == k && neg != 0 && balance[j] > 0) {
+            int min = Math.min(Math.abs(neg), balance[j]);
+            moves += ((long) min * i);
+            neg += min;
         }
 
         return neg == 0 ? moves : -1;
