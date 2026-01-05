4629354acf3e93852802ea8b0bbb9b26b12b0596
Deepanshu8821 <deepanshu@alteredsecurity.com>
2026-01-05 13:40:31 +0530

    added LIS Memoization approach

--- src/main/java/DP/Subsequences/LIS.java
+++ src/main/java/DP/Subsequences/LIS.java
@@ -1,6 +1,45 @@
 package DP.Subsequences;
 
+import java.util.Arrays;
+
 public class LIS {
+
+
+/*
+
+1. method : Recursion (Brute Force)
+
+TC : O(2^N)
+
+i. At each index, you make two recursive calls (take, notTake)
+ii. This creates a binary recursion tree with a depth of n
+
+SC : O(N)
+
+i. The space is determined by the recursion stack (depth of tree would be n)
+
+====================================================================
+
+2. method : Memoization (Top-Down DP)
+
+TC : O(N^2)
+
+i. There are two changing variables in the state: indx (ranges from 0 to n) and prevIndx (ranges from -1 to n-1). The total number of unique states is n x (n+1).
+   Since each state is calculated only once and takes O(1) time to process, the total time is O(n^2).
+ 
+SC : O(N^2) + O(N)
+
+i. To store 2D Dp Array
+ii. Recursive stack space
+
+====================================================================
+
+method : Tabulation (Bottom-Up DP)
+
+TC : O(N^2)
+SC : O(N)
+*
+*/
 
     public static void main(String[] args) {
 
@@ -13,7 +52,13 @@ public class LIS {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
 
-        return lengthOfLisRecursive(nums, n, 0, -1);
+        int[][] dp = new int[n][n+1];
+
+        for (int[] arr : dp) {
+            Arrays.fill(arr, -1);
+        }
+
+        return lengthOfLisMemoized(nums, n, 0, -1, dp);
     }
 
 
@@ -33,5 +78,26 @@ public class LIS {
 
         return Math.max(take, notTake);
     }
+
+    public int lengthOfLisMemoized(int[] nums, int n, int indx, int prevIndx, int[][] dp) {
+
+        if (indx == n) {
+            return 0;
+        }
+
+        if (dp[indx][prevIndx+1] != -1)
+            return dp[indx][prevIndx+1];
+
+        int take = 0;
+
+        if (prevIndx == -1 || nums[prevIndx] < nums[indx]) {
+            take = 1 + lengthOfLisMemoized(nums, n, indx+1, indx, dp);
+        }
+
+        int notTake = lengthOfLisMemoized(nums, n, indx+1, prevIndx, dp);
+
+        return dp[indx][prevIndx + 1] = Math.max(take, notTake);
+    }
+
 
 }
