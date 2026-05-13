566745ce716dffd63071e9f8bd2d8c40bf22cdaa
Deepanshu Tilwani <shonatilwani00@gmail.com>
2026-05-14 00:18:15 +0530

    Added Fruit_Into_Baskets.java and Binary_Subarrays_With_Sum.java

---
+++ src/main/java/LeetCode/SlidingWindow/Medium/Binary_Subarrays_With_Sum.java
@@ -0,0 +1,22 @@
+package LeetCode.SlidingWindow.Medium;
+
+public class Binary_Subarrays_With_Sum {
+
+    public static void main(String[] args) {
+        Binary_Subarrays_With_Sum classObj = new Binary_Subarrays_With_Sum();
+        System.out.println(classObj.numSubarraysWithSum(new int[]{1,0,1,0,1}, 2));
+    }
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int n = nums.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+
+        }
+
+        return count;
+    }
+
+
+}
--- src/main/java/LeetCode/SlidingWindow/Medium/Fruit_Into_Baskets.java
+++ src/main/java/LeetCode/SlidingWindow/Medium/Fruit_Into_Baskets.java
@@ -61,30 +61,4 @@ public class Fruit_Into_Baskets {
         return maxLen;
     }
 
-    public int totalFruitBest(int[] fruits) {
-
-        int n = fruits.length, maxLen = 0, left = 0;
-        Map<Integer, Integer> fruitCount = new HashMap<>();
-
-        for (int right = 0; right < n; right++) {
-
-            fruitCount.put(fruits[right], fruitCount.getOrDefault(fruits[right], 0) + 1);
-
-            while (fruitCount.size() > 2) {
-                int count = fruitCount.get(fruits[left]);
-                if (count > 1) {
-                    fruitCount.put(fruits[left], count - 1);
-                }
-                else {
-                    fruitCount.remove(fruits[left]);
-                }
-                left++;
-            }
-
-            maxLen = Math.max(maxLen, (right - left) + 1);
-        }
-
-        return maxLen;
-    }
-
 }
