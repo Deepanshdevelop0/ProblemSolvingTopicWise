7938a739ee1ba0b3edb1bc0f0a514c5e18fefe5e
Deepanshu Tilwani <shonatilwani00@gmail.com>
2026-05-17 00:30:19 +0530

    Added MergeSort.java

---
+++ src/main/java/LeetCode/Sorting/MergeSort.java
@@ -0,0 +1,95 @@
+package LeetCode.Sorting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+        MergeSort classObj = new MergeSort();
+        int[] arr = new int[]{5,3,2,9,3,4};
+        classObj.mergeSort(arr, 0, arr.length - 1);
+
+        for (int i : arr) {
+            System.out.print(i + ", ");
+        }
+    }
+
+    public void mergeSort(int arr[], int l, int r) {
+
+        if (l >= r) return;
+
+        int mid = l + (r - l) / 2;
+
+        mergeSort(arr, l, mid);
+
+        mergeSort(arr, mid+1, r);
+
+//        mergeIntial(arr, l, mid, r);
+        mergeImproved(arr, l, mid, r); // improved for boxing/unboxing from List type, instead used int[]
+
+    }
+
+
+    void mergeImproved(int[] arr, int l, int mid, int r) {
+
+        int[] res = new int[r - l + 1];
+        int low = l, high = mid+1;
+        int k = 0;
+
+        while (low <= mid && high <= r) {
+
+            if (arr[low] <= arr[high]) {
+                res[k++] = arr[low++];
+            }
+            else {
+                res[k++] = arr[high++];
+            }
+
+        }
+
+        while (low <= mid) {
+            res[k++] = arr[low++];
+        }
+
+        while (high <= r) {
+            res[k++] = arr[high++];
+        }
+
+        for (int i : res) {
+            arr[l++] = i;
+        }
+
+    }
+
+    void merge(int[] arr, int l, int mid, int r) {
+
+        List<Integer> res = new ArrayList<>();
+
+        int low = l, high = mid+1;
+
+        while (low <= mid && high <= r) {
+
+            if (arr[low] <= arr[high]) {
+                res.add(arr[low++]);
+            }
+            else {
+                res.add(arr[high++]);
+            }
+        }
+
+        while (low <= mid) {
+            res.add(arr[low++]);
+        }
+
+        while (high <= r) {
+            res.add(arr[high++]);
+        }
+
+        for (int i : res) {
+            arr[l++] = i;
+        }
+
+    }
+
+}
--- src/main/java/LeetCode/Strings/Problems/miscellaneous/Minimum_Flips_to_Make_Binary_String_Coherent_Contest_Ultimate.java
+++ src/main/java/LeetCode/Strings/Problems/miscellaneous/Minimum_Flips_to_Make_Binary_String_Coherent_Contest_Ultimate.java
@@ -5,6 +5,8 @@ public class Minimum_Flips_to_Make_Binary_String_Coherent_Contest_Ultimate {
     public static void main(String[] args) {
         Minimum_Flips_to_Make_Binary_String_Coherent_Contest_Ultimate classObj = new Minimum_Flips_to_Make_Binary_String_Coherent_Contest_Ultimate();
         System.out.println(classObj.minFlips("011010"));
+        System.out.println(classObj.minFlips("1010"));
+        System.out.println(classObj.minFlips("0110"));
     }
 
     public int minFlips(String s) {
@@ -20,31 +22,39 @@ public class Minimum_Flips_to_Make_Binary_String_Coherent_Contest_Ultimate {
 
         int cost = n;
 
-        cost = Math.min(cost, n - ones); // case 1
+        // Case 1: All ones
+        // Cost is the number of '0's currently in the string that need to be flipped to '1'.
+        cost = Math.min(cost, n - ones);
 
+        // Case 2: All zeros
+        // Cost is the number of '1's currently in the string that need to be flipped to '0'.
         cost = Math.min(cost, ones); // case 2
 
+        // Case 3: Exactly one '1', all others '0'
+        // We flip all current '1's to '0' (cost = ones), then pick the "cheapest" position
+        // to turn into a '1'. If there was already a '1', cost decreases; if not, it increases.
         cost = Math.min(cost, Math.abs(ones - 1)); // case 3
 
-        // case 4
+        // Case 4: Both ends are '1', all middle bits are '0'
         if (n > 1) {
 
             int cornerCaseCost = 0;
 
+            // Count flips needed to make the first and last characters '1'
             if (s.charAt(0) == '0') {
                 cornerCaseCost++;
             }
-
             if (s.charAt(n-1) == '0') {
                 cornerCaseCost++;
             }
 
-            int onesInBetweenCorners = ones - (2 - cornerCaseCost);
+            // Count existing '1's in the middle that must be flipped to '0'
+            int onesInMiddle = ones;
+            if (s.charAt(0) == '1') onesInMiddle--;
+            if (s.charAt(n - 1) == '1') onesInMiddle--;
 
-            cost = Math.min(cost, cornerCaseCost + onesInBetweenCorners);
-
+            cost = Math.min(cost, cornerCaseCost + onesInMiddle);
         }
-
 
         return cost;
     }
