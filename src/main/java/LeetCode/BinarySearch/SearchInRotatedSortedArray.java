b551ab533b49fbcc04eee8c6f262e676fc644a6b
Deepanshu8821 <deepanshu@alteredsecurity.com>
2026-06-10 15:24:33 +0530

    added SearchInRotatedSortedArray

--- src/main/java/LeetCode/BinarySearch/SearchInRotatedSortedArray.java
+++ src/main/java/LeetCode/BinarySearch/SearchInRotatedSortedArray.java
@@ -5,10 +5,15 @@ public class SearchInRotatedSortedArray {
     public static void main(String[] args) {
         int[] nums1 = new int[]{4,5,6,7,0,1,2};
         int[] nums = new int[]{1,2,4,5,6,7,0};
+        int[] nums2 = new int[]{5,1,3};
+        int[] nums3 = new int[]{1,3};
+        int[] nums4 = new int[]{3,1};
 
         SearchInRotatedSortedArray classObj = new SearchInRotatedSortedArray();
 
-        classObj.search(nums, 0);
+        int res = classObj.searchI(nums4, 1);
+
+        System.out.println(res);
 
     }
 
@@ -44,6 +49,41 @@ public class SearchInRotatedSortedArray {
                     right = mid - 1;
                 }
             }
+        }
+
+        return -1;
+    }
+
+
+    // Practice
+    public int searchI(int[] nums, int target) {
+
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[left] <= nums[mid]) { // left half is sorted
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+            else {  // right half is sorted
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
+
         }
 
         return -1;
