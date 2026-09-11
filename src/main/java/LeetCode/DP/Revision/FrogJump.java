5a76ec3bb6c089307a7406e1f99050fcb68e1f2d
Deepanshu8821 <deepanshu@alteredsecurity.com>
2026-09-11 18:54:55 +0530

    added DP problems

--- src/main/java/LeetCode/DP/Revision/FrogJump.java
+++ src/main/java/LeetCode/DP/Revision/FrogJump.java
@@ -14,13 +14,12 @@ public class FrogJump {
 
         int[] stones = new int[]{0, 1, 3, 5, 6, 8, 12, 17};
         int[] stones1 = new int[]{0, 1, 2, 3, 4, 8, 9, 11};
-        System.out.println(frogJump.canCrossMemoization(stones));
+        System.out.println(frogJump.canCrossTabulation(stones));
 
     }
 
-    /* Brute Force Recursion Approach */
+    /* 1. Brute Force Recursion Approach */
     public boolean canCross(int[] stones) {
-
         if (stones[1] != 1) return false;
         if (stones.length == 2) return true;
 
@@ -30,40 +29,37 @@ public class FrogJump {
             stoneIndxMap.put(stones[i], i);
         }
 
-        return canCrossRecursive(1, 1, stones, stoneIndxMap);
+        return crossRecursively(1, 1, stones, stoneIndxMap);
     }
 
-    public boolean canCrossRecursive(int currIndx, int k, int[] stones, Map<Integer, Integer> stoneIndxMap) {
-        if (currIndx > stones.length) {
-            return false;
-        }
-        if (currIndx == stones.length - 1) {
+    public boolean crossRecursively(int currIndx, int k, int[] stones, Map<Integer, Integer> stoneIndxMap) {
+        if (currIndx == stones.length-1) {
             return true;
         }
+
         boolean res = false;
 
         if (stoneIndxMap.containsKey(stones[currIndx] + k)) {
             int nextIndx = stoneIndxMap.get(stones[currIndx] + k);
-            res = canCrossRecursive(nextIndx, k, stones, stoneIndxMap);
+            res = crossRecursively(nextIndx, k, stones, stoneIndxMap);
         }
 
-        if (stoneIndxMap.containsKey(stones[currIndx] + (k + 1))) {
+        if (!res && stoneIndxMap.containsKey(stones[currIndx] + (k + 1))) {
             int nextIndx = stoneIndxMap.get(stones[currIndx] + (k + 1));
-            res = res || canCrossRecursive(nextIndx, k + 1, stones, stoneIndxMap);
+            res = crossRecursively(nextIndx, k + 1, stones, stoneIndxMap);
         }
 
-        if (k - 1 > 0 && stoneIndxMap.containsKey(stones[currIndx] + (k - 1))) {
-            int nextIndx = stoneIndxMap.get(stones[currIndx] + (k - 1));
-            res = res || canCrossRecursive(nextIndx, k - 1, stones, stoneIndxMap);
+        if (!res && k-1 > 0 && stoneIndxMap.containsKey(stones[currIndx] + (k - 1))) {
+            int nextIndx = stoneIndxMap.get(stones[currIndx] + (k-1));
+            res = crossRecursively(nextIndx, k-1, stones, stoneIndxMap);
         }
 
         return res;
     }
 
 
-    /* Memoization Approach */
+    /* 2. Memoization Approach */
     public boolean canCrossMemoization(int[] stones) {
-
         if (stones[1] != 1) return false;
         if (stones.length == 2) return true;
 
@@ -75,10 +71,10 @@ public class FrogJump {
 
         Boolean[][] dp = new Boolean[stones.length][stones.length + 1];
 
-        return canCrossRecursiveMemoization(1, 1, stones, stoneIndxMap, dp);
+        return crossRecursivelyMemoization(1, 1, stones, stoneIndxMap, dp);
     }
 
-    public boolean canCrossRecursiveMemoization(int currIndx, int k, int[] stones, Map<Integer, Integer> stoneIndxMap, Boolean[][] dp) {
+    public boolean crossRecursivelyMemoization(int currIndx, int k, int[] stones, Map<Integer, Integer> stoneIndxMap, Boolean[][] dp) {
         if (currIndx == stones.length - 1) {
             return true;
         }
@@ -91,17 +87,65 @@ public class FrogJump {
 
         if (stoneIndxMap.containsKey(stones[currIndx] + k)) {
             int nextIndx = stoneIndxMap.get(stones[currIndx] + k);
-            res = canCrossRecursiveMemoization(nextIndx, k, stones, stoneIndxMap, dp);
+            res = crossRecursivelyMemoization(nextIndx, k, stones, stoneIndxMap, dp);
         }
 
         if (!res && stoneIndxMap.containsKey(stones[currIndx] + (k + 1))) {
             int nextIndx = stoneIndxMap.get(stones[currIndx] + (k + 1));
-            res = canCrossRecursiveMemoization(nextIndx, k + 1, stones, stoneIndxMap, dp);
+            res = crossRecursivelyMemoization(nextIndx, k+1, stones, stoneIndxMap, dp);
         }
 
-        if (!res && k - 1 > 0 && stoneIndxMap.containsKey(stones[currIndx] + (k - 1))) {
+        if (!res && k-1 > 0 && stoneIndxMap.containsKey(stones[currIndx] + (k - 1))) {
             int nextIndx = stoneIndxMap.get(stones[currIndx] + (k - 1));
-            res = canCrossRecursiveMemoization(nextIndx, k - 1, stones, stoneIndxMap, dp);
+            res = crossRecursivelyMemoization(nextIndx, k-1, stones, stoneIndxMap, dp);
+        }
+
+        dp[currIndx][k] = res;
+
+        return res;
+    }
+
+
+    /* 3. Tabulation Approach */
+    public boolean canCrossTabulation(int[] stones) {
+        if (stones[1] != 1) return false;
+        if (stones.length == 2) return true;
+
+        Map<Integer, Integer> stoneIndxMap = new HashMap<>();
+
+        for (int i = 0; i < stones.length; i++) {
+            stoneIndxMap.put(stones[i], i);
+        }
+
+        Boolean[][] dp = new Boolean[stones.length][stones.length + 1];
+
+        return crossRecursivelyTabulation(1, 1, stones, stoneIndxMap, dp);
+    }
+
+    public boolean crossRecursivelyTabulation(int currIndx, int k, int[] stones, Map<Integer, Integer> stoneIndxMap, Boolean[][] dp) {
+        if (currIndx == stones.length - 1) {
+            return true;
+        }
+
+        if (dp[currIndx][k] != null) {
+            return dp[currIndx][k];
+        }
+
+        boolean res = false;
+
+        if (stoneIndxMap.containsKey(stones[currIndx] + k)) {
+            int nextIndx = stoneIndxMap.get(stones[currIndx] + k);
+            res = crossRecursivelyTabulation(nextIndx, k, stones, stoneIndxMap, dp);
+        }
+
+        if (!res && stoneIndxMap.containsKey(stones[currIndx] + (k + 1))) {
+            int nextIndx = stoneIndxMap.get(stones[currIndx] + (k + 1));
+            res = crossRecursivelyTabulation(nextIndx, k+1, stones, stoneIndxMap, dp);
+        }
+
+        if (!res && k-1 > 0 && stoneIndxMap.containsKey(stones[currIndx] + (k - 1))) {
+            int nextIndx = stoneIndxMap.get(stones[currIndx] + (k - 1));
+            res = crossRecursivelyTabulation(nextIndx, k-1, stones, stoneIndxMap, dp);
         }
 
         dp[currIndx][k] = res;
