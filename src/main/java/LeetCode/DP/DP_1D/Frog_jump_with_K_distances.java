cb76f3c3a6e27124601ea807564e1dcebed42c38
Deepanshu Tilwani <shonatilwani00@gmail.com>
2026-09-22 00:47:33 +0530

    Added Recursion solution

--- src/main/java/LeetCode/DP/DP_1D/Frog_jump_with_K_distances.java
+++ src/main/java/LeetCode/DP/DP_1D/Frog_jump_with_K_distances.java
@@ -1,6 +1,8 @@
 package LeetCode.DP.DP_1D;
 
 
+import java.util.Arrays;
+
 public class Frog_jump_with_K_distances {
 
     public static void main(String[] args) {
@@ -10,33 +12,52 @@ public class Frog_jump_with_K_distances {
         int[] heights = new int[]{10, 5, 20, 0, 15};
         int[] heights1 = new int[]{15, 4, 1, 14, 15};
         System.out.println(frogJumpWithKDistances.frogJump(heights, 2));
+//        System.out.println(frogJumpWithKDistances.frogJump(heights1, 3));
 
     }
 
     public int frogJump(int[] heights, int k) {
+        int n = heights.length;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        dp[n-1] = 0;
 
-        int min = Integer.MAX_VALUE;
-
-//        for (int i = 1; i < heights.length && i <= k; i++) {
-//            min = Math.min(min, jumpRecursive(heights, k, i));
-//        }
-
-        return jumpRecursive(heights, k, 0);
+//        return jumpRecursive(heights, k, heights.length, 0);
+        return jumpRecursiveMemoized(heights, k, heights.length, 0, dp);
     }
 
-    public int jumpRecursive(int[] heights, int k, int indx) {
-        if (indx == heights.length - 1) {
-            return heights[indx];
+    public int jumpRecursive(int[] heights, int k, int n, int indx) {
+        if (indx == n - 1) {
+            return 0;
         }
 
         int min = Integer.MAX_VALUE;
 
-        for (int i = 1; i <= k && indx+i < heights.length; i++) {
-            int next = jumpRecursive(heights, k, indx+i);
-            min = Math.min(min, Math.abs(heights[indx] - next));
+        for (int i = 1; i <= k && indx + i < n; i++) {
+            int next = jumpRecursive(heights, k, n, indx + i);
+            int diff = Math.abs(heights[indx] - heights[indx + i]);
+            min = Math.min(min, diff + next);
         }
 
         return min;
+    }
+
+
+    public int jumpRecursiveMemoized(int[] heights, int k, int n, int indx, int[] dp) {
+
+        if (dp[indx] != -1) {
+            return dp[indx];
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 1; i <= k && i + indx < n; i++) {
+            int next = jumpRecursiveMemoized(heights, k, n, indx + i, dp);
+            int diff = Math.abs(heights[indx] - heights[indx + i]);
+            min = Math.min(min, next + diff);
+        }
+
+        return dp[indx] = min;
     }
 
 }
